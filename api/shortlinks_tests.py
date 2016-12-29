1ca7a7e6e518667ba70f472333b176c528e5acd4
SohilaBogdady <sohila.bogdady2012@gmailcom>
2016-12-30 00:45:33 +0200

    Commit in shortlinks_test.py, finish unit test for  get  request

--- api/shortlinks_tests.py
+++ api/shortlinks_tests.py
@@ -5,21 +5,24 @@ from app import app
 class ShortLinkTestCase (unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
-        #app.config['TESTING'] = True
         self.app = app.test_client()
-       # with flaskr.app.app_context():
-        #    flaskr.init_db()
-    def test_empty_doc(self):
-        rv =self.app.get('/shortlinks',follow_redirects=True)
-        #print (rv.data)
-    def JSONReq (self):
+    def test_nonAuth(self):
+        """ non authorized user request """
+        rv =self.app.get('/shortlinks',headers={'Content-Type': 'application/json'})
+        print (rv.data)
+    def test_nonJSON (self):
+        re= self.app.get ('/shortlinks',headers={'Authorization':'Basic YWRtaW46YWRtaW4='})
+        print (re.data)
+    def JSONReqAuth (self):
+         """add  content type : application-json and  authorizaton :encode (user,pass)"""
          authorizedUser = {"user": "admin", "password": "admin"}
-         headers = {'Content-Type': 'application/json'}
+         headers = {'Content-Type': 'application/json', 'Authorization':'Basic YWRtaW46YWRtaW4='}
          return headers
-    def test_JSONRequestGet(self):
+    def test_JSONAuthRequestGet(self):
         """send json request get method"""
-        resp= self.app.get ('/shortlinks',headers=self.JSONReq() )
+        resp= self.app.get ('/shortlinks',headers=self.JSONReqAuth() )
         print (resp.data)
+
 
 
 
