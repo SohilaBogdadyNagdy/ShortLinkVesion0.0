1888a831d600a06001f8177b28250fe1a58c3753
SohilaBogdady <sohila.bogdady2012@gmailcom>
2016-12-30 15:35:00 +0200

    create testcase for post request inshortlinks_test.py

--- api/shortlinks_tests.py
+++ api/shortlinks_tests.py
@@ -1,5 +1,6 @@
 import unittest
 from app import app
+import json
 
 
 class ShortLinkTestCase (unittest.TestCase):
@@ -9,10 +10,10 @@ class ShortLinkTestCase (unittest.TestCase):
     def test_nonAuth(self):
         """ non authorized user request """
         rv =self.app.get('/shortlinks',headers={'Content-Type': 'application/json'})
-        print (rv.data)
+        #print (rv.data)
     def test_nonJSON (self):
         re= self.app.get ('/shortlinks',headers={'Authorization':'Basic YWRtaW46YWRtaW4='})
-        print (re.data)
+        #print (re.data)
     def JSONReqAuth (self):
          """add  content type : application-json and  authorizaton :encode (user,pass)"""
          authorizedUser = {"user": "admin", "password": "admin"}
@@ -21,7 +22,33 @@ class ShortLinkTestCase (unittest.TestCase):
     def test_JSONAuthRequestGet(self):
         """send json request get method"""
         resp= self.app.get ('/shortlinks',headers=self.JSONReqAuth() )
-        print (resp.data)
+        #print (resp.data)
+    def test_POST(self):
+        response= self.app.post ('/shortlinks')
+        #print (response.data)
+    def test_AuthPost(self):
+
+        myresp = self.app.post ('/shortlinks',headers={'Authorization':'Basic YWRtaW46YWRtaW4='})
+        print (myresp.data)
+    def test_AuthJsonPost(self):
+        """trigger new post request create and add new shortlink document in collection """
+        NewSH={
+        "slug": "s5G1f3",
+        "ios": {
+        "primary": "IOSPRIMARY16",
+        "fallback": "FALLBACKIOS16"
+        },
+        "android": {
+        "primary": "ANDpRIMARY16",
+        "fallback": "andFALLBACK16"
+        },
+        "web": "WEB16"
+        }
+        myheaders= { 'Content-Type':'application/json','Authorization':'Basic YWRtaW46YWRtaW4='}
+        #myresp = self.app.post('/shortlinks',data=json.dumps (NewSH),headers=myheaders)
+        #print (myresp.data)
+
+
 
 
 
